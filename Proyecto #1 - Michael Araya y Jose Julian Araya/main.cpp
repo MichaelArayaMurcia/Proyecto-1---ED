ea53b85cf762fa86c8cff4b5cdca8d1af3bc7696
MichaelArayaMurcia <michaelarayamurcia@gmail.com>
2020-12-06 19:59:56 -0600

    Poniendo el archivo con las palabras y poniendo un código de prueba.

--- Proyecto #1 - Michael Araya y Jose Julian Araya/main.cpp
+++ Proyecto #1 - Michael Araya y Jose Julian Araya/main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 #include "Trie.h"
+#include <fstream>
+#include <Windows.h>
 
 using namespace std;
 
 int main()
 {
+    setlocale(LC_ALL, "spanish");
+    SetConsoleCP(1252);
+    SetConsoleOutputCP(1252);
+
+    string myText;
+    // Read from the text file
+    ifstream MyReadFile("ignorar.txt");
+
+    // Use a while loop together with the getline() function to read the file line by line
+    while (getline (MyReadFile, myText)) {
+        // Output the text from the file
+        cout << myText;
+    }
+
+    // Close the file
+    MyReadFile.close();
     cout << "Hola :3" << endl;
     return 0;
 }
