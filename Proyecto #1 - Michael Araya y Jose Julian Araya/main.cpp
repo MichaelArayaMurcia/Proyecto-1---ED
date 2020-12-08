797357e945919d56cd7f296ecff77b906de36b36
MichaelArayaMurcia <michaelarayamurcia@gmail.com>
2020-12-08 12:41:10 -0600

    Completando los objetivos 1,2 y 3 del proyecto.

--- Proyecto #1 - Michael Araya y Jose Julian Araya/main.cpp
+++ Proyecto #1 - Michael Araya y Jose Julian Araya/main.cpp
@@ -11,18 +11,35 @@ int main()
     SetConsoleCP(1252);
     SetConsoleOutputCP(1252);
 
+    cout << "Bienvenido!" << endl;
+    cout << "Este programa usa la estructura de datos AVL para analizar datos sobre un archivo" << endl;
+
     string myText;
     // Read from the text file
-    ifstream MyReadFile("ignorar.txt");
 
-    // Use a while loop together with the getline() function to read the file line by line
-    while (getline (MyReadFile, myText)) {
-        // Output the text from the file
-        cout << myText;
+    string nombreArchivo = "";
+
+    cout << "Ingrese el nombre del archivo que va a utilizar" << endl;
+    cin >> nombreArchivo;
+
+
+        ifstream MyReadFile(nombreArchivo + ".txt");
+    if(MyReadFile.fail()){
+        cout << "No es posible abrir el archivo" << endl;
+    }else {
+        int i = 1;
+
+        while (getline (MyReadFile, myText)) {
+            // Output the text from the file
+            cout << myText << endl;
+            i += 1;
+        }
+
+        cout << i;
+
+        // Close the file
+        MyReadFile.close();
     }
-
-    // Close the file
-    MyReadFile.close();
     cout << "Hola :3" << endl;
     return 0;
 }
